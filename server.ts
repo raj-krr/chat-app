fe5a9f0e457d37f5fbf8b5e9b7b1a12873a72f57
raj-krr <mr.rajkumar3579@gmail.com>
2025-10-06 11:36:18 +0530

    added frontend

---
+++ server.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
+
+interface ChatMessage {
+  user: string;
+  text: string;
+  time: string;
+}
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join("./public")));
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join( "../public/index.html"));
+});
+
+
+io.on("connection", (socket: Socket) => {
+  console.log(" A user connected");
+
+  socket.on("user-message", (data: ChatMessage) => {
+    console.log(` ${data.user}: ${data.text}`);
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(" User disconnected");
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
