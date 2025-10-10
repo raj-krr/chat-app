698372c2127e7776675a0445bc06f6c365502e4b
raj-krr <mr.rajkumar3579@gmail.com>
2025-10-10 22:39:11 +0530

    added gitignore

--- server.ts
+++ server.ts
@@ -1,39 +1,39 @@
-import express, { Request, Response } from "express";
-import http from "http";
-import { Server, Socket } from "socket.io";
-import path from "path";
+// import express, { Request, Response } from "express";
+// import http from "http";
+// import { Server, Socket } from "socket.io";
+// import path from "path";
 
-interface ChatMessage {
-  user: string;
-  text: string;
-  time: string;
-}
+// interface ChatMessage {
+//   user: string;
+//   text: string;
+//   time: string;
+// }
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
 
-app.use(express.static(path.join("./public")));
+// app.use(express.static(path.join("./public")));
 
-app.get("/", (req: Request, res: Response) => {
-  res.sendFile(path.join( "../public/index.html"));
-});
+// app.get("/", (req: Request, res: Response) => {
+//   res.sendFile(path.join( "../public/index.html"));
+// });
 
 
-io.on("connection", (socket: Socket) => {
-  console.log(" A user connected");
+// io.on("connection", (socket: Socket) => {
+//   console.log(" A user connected");
 
-  socket.on("user-message", (data: ChatMessage) => {
-    console.log(` ${data.user}: ${data.text}`);
-    io.emit("message", data);
-  });
+//   socket.on("user-message", (data: ChatMessage) => {
+//     console.log(` ${data.user}: ${data.text}`);
+//     io.emit("message", data);
+//   });
 
-  socket.on("disconnect", () => {
-    console.log(" User disconnected");
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log(" User disconnected");
+//   });
+// });
 
-server.listen(5000, "0.0.0.0", () => {
-  console.log(`Server running on http://13.127.60.12:5000`);
-});
+// server.listen(5000, () => {
+//   console.log(`Server running on 5000`);
+// });
 
